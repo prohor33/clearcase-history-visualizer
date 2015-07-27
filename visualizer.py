c31efa9e38c6d3279921d9869cec08fd8f3169ef
prohor33 <prohor33@gmail.com>
2015-07-27 13:55:26 +0300

    start from double_pendulum_animated.py sample

---
+++ visualizer.py
@@ -0,0 +1,81 @@
+from numpy import sin, cos
+import numpy as np
+import matplotlib.pyplot as plt
+import matplotlib.animation as animation
+
+class DoublePendulum:
+    """Double Pendulum Class
+
+    init_state is [theta1, omega1, theta2, omega2] in degrees,
+    where theta1, omega1 is the angular position and velocity of the first
+    pendulum arm, and theta2, omega2 is that of the second pendulum arm
+    """
+    def __init__(self,
+                 init_state = [120, 0, -20, 0],            
+                 origin=(0, 0)): 
+        self.init_state = np.asarray(init_state, dtype='float')
+        self.origin = origin
+        self.time_elapsed = 0
+
+        self.state = self.init_state * np.pi / 180.
+    
+    def position(self):
+        """compute the current x,y positions of the pendulum arms"""
+
+        return (1, 1)
+
+    def step(self, dt):
+        """execute one time step of length dt and update state"""
+        self.state = 1
+        self.time_elapsed += dt
+
+#------------------------------------------------------------
+# set up initial state and global variables
+pendulum = DoublePendulum()
+dt = 1./30 # 30 fps
+
+#------------------------------------------------------------
+# set up figure and animation
+fig = plt.figure()
+ax = fig.add_subplot(111, aspect='equal', autoscale_on=False,
+                     xlim=(-2, 2), ylim=(-2, 2))
+ax.grid()
+
+line, = ax.plot([], [], 'o-', lw=2)
+time_text = ax.text(0.02, 0.95, '', transform=ax.transAxes)
+energy_text = ax.text(0.02, 0.90, '', transform=ax.transAxes)
+
+def init():
+    """initialize animation"""
+    line.set_data([], [])
+    time_text.set_text('')
+    energy_text.set_text('')
+    return line, time_text, energy_text
+
+def animate(i):
+    """perform animation step"""
+    global pendulum, dt
+    pendulum.step(dt)
+    
+    line.set_data(*pendulum.position())
+    time_text.set_text('time = %.1f' % pendulum.time_elapsed)
+    return line, time_text, energy_text
+
+# choose the interval based on dt and the time to animate one step
+from time import time
+t0 = time()
+animate(0)
+t1 = time()
+interval = 1000 * dt - (t1 - t0)
+
+ani = animation.FuncAnimation(fig, animate, frames=300,
+                              interval=interval, blit=True, init_func=init)
+
+# save the animation as an mp4.  This requires ffmpeg or mencoder to be
+# installed.  The extra_args ensure that the x264 codec is used, so that
+# the video can be embedded in html5.  You may need to adjust this for
+# your system: for more information, see
+# http://matplotlib.sourceforge.net/api/animation_api.html
+#ani.save('double_pendulum.mp4', fps=30, extra_args=['-vcodec', 'libx264'])
+
+plt.show()
